bd4c2c9f93c39220bcfb109969cca8ee7b29e494
student-pro <marktokhtobin@gmail.com>
2026-02-06 15:02:59 +0700

    Отображение товаров 1

--- examapp/hash.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-from django.contrib.auth.hashers import make_password
-settings.configure()
-password = "gynQMT"
-hashed_password = make_password(password)
-print(hashed_password)
